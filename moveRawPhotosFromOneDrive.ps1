ab3f2ca7b0b17c56235e4ac378197bfd86a14624
marty-hope <marty.hope@gmail.com>
2019-02-02 17:08:46 -0800

    Add PowerShell to move raw photos to SD card storage.

---
+++ moveRawPhotosFromOneDrive.ps1
@@ -0,0 +1,13 @@
+$originalLocation = "C:\Users\marty\OneDrive\Pictures\Camera imports\2019-02"
+$copyToLocation = "D:\Pictures\Camera Roll\02-02-2019"
+if([System.IO.Directory]::Exists($originalLocation)){
+    $files = [System.IO.Directory]::EnumerateFiles($originalLocation, "*.arw")
+    foreach($file in $files)
+    {
+        $fileNameWithoutPath = [System.IO.Path]::GetFileName($file)
+        $newFileNameWithFullPath = "$copyToLocation\$fileNameWithoutPath"
+        [System.IO.File]::Move($file, $newFileNameWithFullPath)
+
+    }
+     
+}
